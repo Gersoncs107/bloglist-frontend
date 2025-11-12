cd9184cbcc8f47325f66ac947856683ee2defd8d
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-11 22:13:10 -0300

    bloglist-frontend update

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ const App = () => {
 
   return (
     <div>
+      <h2>Log in to application</h2>
+      
+
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
