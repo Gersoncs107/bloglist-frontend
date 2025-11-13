b469b393c09a6a69a2aa711815abd771fcc2d1fb
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-12 22:17:03 -0300

    bloglist-frontend update

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,13 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
@@ -21,4 +28,4 @@ const update = (id, newObject) => {
 };
 
 
-export default { getAll, create, update }
+export default { getAll, create, update, setToken }
