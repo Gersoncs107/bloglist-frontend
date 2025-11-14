a09a9b368f439afcaec7e7c574dc41d7d15d12a9
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-13 22:06:16 -0300

    bloglist-frontend update

--- src/App.jsx
+++ src/App.jsx
@@ -99,15 +99,57 @@ const App = () => {
   )
 
   const blogForm = () => (
+  <div>
+    <h2>Create new blog</h2>
     <form onSubmit={addBlog}>
-      <input
-        value={newBlog}
-        onChange={handleBlogChange}
-      />
-      <button type="submit">save</button>    
+      <div>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+            placeholder="Enter blog title"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Author:
+          <input
+            type="text"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+            placeholder="Enter author name"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          URL:
+          <input
+            type="url"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+            placeholder="https://example.com"
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Likes:
+          <input
+            type="number"
+            value={likes}
+            onChange={({ target }) => setLikes(Number(target.value))}
+            min="0"
+          />
+        </label>
+      </div>
+      <button type="submit">Create</button>
     </form>
-    
-  )
+  </div>
+)
 
   return (
     <div>
