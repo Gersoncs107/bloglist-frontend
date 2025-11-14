5cab267a48e2d4332a108dcb5c120dee2b2837e3
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-13 21:45:25 -0300

    bloglist-frontend update

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,12 @@ const App = () => {
         onChange={handleBlogChange}
       />
       <button type="submit">save</button>
-    </form>  
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </form>
+    
   )
 
   return (
@@ -111,7 +116,7 @@ const App = () => {
       </div>
     }
 
-      <h2>Log in to application</h2>
+      {/* <h2>Log in to application</h2> */}
       {/* <form onSubmit={handleLogin}>
         <div>
           username
@@ -134,10 +139,10 @@ const App = () => {
         <button type="submit">login</button>
       </form> */}
 
-      <h2>blogs</h2>
+      {/* <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
-      )}
+      )} */}
     </div>
   )
 }
