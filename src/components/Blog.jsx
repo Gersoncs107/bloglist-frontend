1056834da1f7946070e76cf32b7da3abd86a66ea
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-23 21:25:37 -0300

    bloglist-frontend update

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,15 @@
 const Blog = ({ blog }) => {
-   
+   const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
 return (
   
-  <div className="blog">
+  <div className="blog" style={blogStyle}>
     <strong>{blog.title}</strong> by {blog.author}
   </div>
 )}
