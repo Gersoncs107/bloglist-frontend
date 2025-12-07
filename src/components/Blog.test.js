d516ebee0eef8b61f3c748d4e3da18c3a175a393
Gersoncs107 <gerson.silva107@gmail.com>
2025-12-06 21:57:23 -0300

    bloglist-frontend update

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -2,3 +2,22 @@ import React from "react";
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from "@testing-library/react";
 import Blog from "./Blog";
+
+const mockUpdateBlog = jest.fn();
+const mockDeleteBlog = jest.fn();
+
+describe("<Blog />", () => {
+    let container
+
+    const blog = {
+        tittle: "Test Blog Title",
+        authir: "Test Author",
+        url: "http://testblog.com",
+        likes: 5,
+        user: {
+            username: "testuser",
+            name: "Test User"
+        }
+    }
+
+})
