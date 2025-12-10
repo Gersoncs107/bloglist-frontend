93bb852b377535d98a7b9ec01335201cc3124756
Gersoncs107 <gerson.silva107@gmail.com>
2025-12-09 22:32:38 -0300

    bloglist-frontend update

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
-// babel.config.js
 export default {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }]
+    ['@babel/preset-react', {
+      runtime: 'automatic' 
+    }]
   ],
 }
