ab85bd2af6add7ce897da2854f7281b4fc8a7b1a
Gersoncs107 <gerson.silva107@gmail.com>
2025-12-29 21:55:41 -0300

    bloglist-frontend update

--- cypress/e2e/bloglist_app.cy.js
+++ cypress/e2e/bloglist_app.cy.js
@@ -1,7 +1,7 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:5173/')
   })
 
   it('Login form is shown', function() {
