b04bcac315e291d028af3e568c9092ffc18f62bb
Gersoncs107 <gerson.silva107@gmail.com>
2026-01-01 21:59:32 -0300

    bloglist-frontend update

--- cypress/e2e/bloglist_app.cy.js
+++ cypress/e2e/bloglist_app.cy.js
@@ -2,9 +2,9 @@ describe('Blog app', () => {
   beforeEach(() => {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
     const user = {
-      name: 'Test User',
-      username: 'testuser',
-      password: 'password123'
+      name: 'Superuser',
+      username: 'root',
+      password: 'salainen'
     }
     cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:5173/')
@@ -14,5 +14,25 @@ describe('Blog app', () => {
     cy.visit('http://localhost:5173/')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login', () => {
+    it('succeeds with correct credentials', () => {
+      cy.get('#username').type('root')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+      cy.contains('Superuser logged in')
+    })
+
+    it('fails with wrong credentials', () => {
+      cy.get('#username').type('root')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.get('.notification')
+        .should('contain', 'Wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+      cy.get('html').should('not.contain', 'Superuser logged in')
+    })
   })
 })
