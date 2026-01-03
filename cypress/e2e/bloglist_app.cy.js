b252f94955c3ac0d6faf825be11c63f7bf9ecde4
Gersoncs107 <gerson.silva107@gmail.com>
2026-01-03 19:36:11 -0300

    bloglist-frontend update

--- cypress/e2e/bloglist_app.cy.js
+++ cypress/e2e/bloglist_app.cy.js
@@ -32,4 +32,20 @@ describe('Blog app', () => {
       cy.get('html').should('not.contain', 'Superuser logged in')
     })
   })
+  
+  describe('When logged in', () => {
+    beforeEach(() => {
+      cy.get('#username').type('root')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+    })
+    it('A blog can be created', () => {
+      cy.contains('Create New Blog').click()
+      cy.get('input[placeholder="Enter blog title"]').type('A blog created by cypress')
+      cy.get('input[placeholder="Enter author name"]').type('Cypress Author')
+      cy.get('input[placeholder="https://example.com"]').type('https://cypress.io')
+      cy.contains('Create').click()
+      cy.contains('A blog created by cypress Cypress Author')
+    })
+  })
 })
