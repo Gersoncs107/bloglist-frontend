2ecaaccb1f67490d68999939349bc076765ad746
Gersoncs107 <gerson.silva107@gmail.com>
2026-03-28 22:19:51 -0300

    bloglist-frontend update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { notify } from './reducers/notificationReducer'
+import { initializeBlogs, createBlog } from './reducers/blogReducer'
 import Blog from './components/Blog'
 import './App.css'
 import Notification from './components/Notification'
