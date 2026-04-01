53e6247ffce984b2154f4de745fd4c0bdfbe3af8
Gersoncs107 <gerson.silva107@gmail.com>
2026-03-31 22:29:20 -0300

    bloglist-frontend update

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit'
+import blogService from '../services/blogs'
+import loginService from '../services/login'
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: null,
+  reducers: {
+    setUser(state, action) {
+      return action.payload
+    },
+    clearUser() {
+      return null
+    }
+  }
+})
+
+export const { setUser, clearUser } = userSlice.actions
+
+// Thunk: inicializa usuário a partir do localStorage
+export const initializeUser = () => dispatch => {
+  const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+  if (loggedUserJSON) {
+    const user = JSON.parse(loggedUserJSON)
+    dispatch(setUser(user))
+    blogService.setToken(user.token)
+  }
+}
+
+// Thunk: faz login, persiste no localStorage e atualiza a store
+export const loginUser = (credentials) => async dispatch => {
+  const user = await loginService.login(credentials)
+  window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
+  blogService.setToken(user.token)
+  dispatch(setUser(user))
+  return user
+}
+
+// Thunk: faz logout, limpa localStorage e a store
+export const logoutUser = () => dispatch => {
+  window.localStorage.removeItem('loggedBlogAppUser')
+  dispatch(clearUser())
+}
+
+export default userSlice.reducer
