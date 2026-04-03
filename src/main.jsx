1b503e53209ce0424558ecd31af6bb6fade3d5ea
Gersoncs107 <gerson.silva107@gmail.com>
2026-04-02 21:50:20 -0300

    bloglist-frontend update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { notificationProvider } from './contexts/NotificationContext'
 import App from './App'
 import store from './store'
 
