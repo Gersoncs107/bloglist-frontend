32affb30258e996d891dd666ad1a10f4a6ff71c9
Gersoncs107 <gerson.silva107@gmail.com>
2026-04-02 21:40:32 -0300

    bloglist-frontend update

--- src/contexts/NotificationContext.js
+++ src/contexts/NotificationContext.js
@@ -2,6 +2,16 @@ import { createContext, useContext, useReducer } from "react";
 
 const notificationContext = createContext()
 
+const notificationReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET':
+            return action.payload
+        case 'CLEAR':
+            return null
+        default:
+            return state
+    }
+}
 
 
 export default notificationContext
