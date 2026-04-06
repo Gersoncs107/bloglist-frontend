7a937d945d0e458e46fab7bdc1fc28d91ee372b1
Gersoncs107 <gerson.silva107@gmail.com>
2026-04-05 21:47:53 -0300

    bloglist-frontend update

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -3,3 +3,15 @@ import { blogService } from "../services/blogs";
 import { userService } from "../services/users";
 
 const UserContext = createContext()
+
+const usereducer = (state, action) => {
+    switch (action.type) {
+        case 'SET':
+            return action.payload
+        case 'CLEAR':
+            return null
+        default:
+            return state
+    }
+
+}
