8a6d7ebb0b9d8ca5af1c839cc05a35ac5d06656d
Gersoncs107 <gerson.silva107@gmail.com>
2026-04-11 19:29:45 -0300

    bloglist-frontend update

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NotificationProvider } from './contexts/NotificationContext.jsx'
 import { UserProvider } from './contexts/UserContext.jsx'
@@ -7,11 +8,13 @@ import App from './App'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <UserProvider>
-      <NotificationProvider>
-        <App />
-      </NotificationProvider>
-    </UserProvider>
-  </QueryClientProvider>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <NotificationProvider>
+          <App />
+        </NotificationProvider>
+      </UserProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
 )
