f66241ba30983c05606c3df86937ef7a819caa18
Gersoncs107 <gerson.silva107@gmail.com>
2026-04-11 22:25:22 -0300

    bloglist-frontend update

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,52 +1,21 @@
-import { Link } from "react-router-dom"                 
+import { Link } from 'react-router-dom'    // ← novo
 
 const Blog = ({ blog }) => {
-  
-return (
-    <div className="blog">
-      <div style={{ marginBottom: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <div>
-          <strong>{blog.title}</strong> by <em>{blog.author}</em>
-        </div>
-        <button id="view-button" onClick={toggleVisibility}>
-          {visible ? 'hide' : 'view'}
-        </button>
+  return (
+    <div className="blog" style={{
+      padding: '10px 0',
+      borderBottom: '1px solid #e0e0e0',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    }}>
+      <div>
+        <Link to={`/blogs/${blog.id}`} style={{ fontWeight: 'bold', marginRight: '8px' }}>
+          {blog.title}
+        </Link>
+        <em>by {blog.author}</em>
       </div>
-
-      {visible && (
-        <div style={{ marginTop: '10px', paddingLeft: '10px' }}>
-          <div>
-            <span style={labelStyle}>URL:</span>
-            <a href={blog.url} target="_blank" rel="noopener noreferrer">{blog.url}</a>
-          </div>
-          <div style={{ marginTop: '8px' }}>
-            <span style={labelStyle}>Likes:</span> {blog.likes}
-            <button
-              id="like-button"
-              style={{ marginLeft: '10px' }}
-              onClick={handleLike}
-              disabled={likeMutation.isPending}
-            >
-              like
-            </button>
-          </div>
-          <div style={{ marginTop: '8px' }}>
-            <span style={labelStyle}>Added by:</span> {blog.user?.name || 'unknown'}
-          </div>
-          {canRemove && (
-            <div style={{ marginTop: '12px' }}>
-              <button
-                id="remove-button"
-                style={{ backgroundColor: '#d9534f', color: 'white' }}
-                onClick={handleRemove}
-                disabled={deleteMutation.isPending}
-              >
-                Remove
-              </button>
-            </div>
-          )}
-        </div>
-      )}
+      <span style={{ color: '#555' }}>{blog.likes} likes</span>
     </div>
   )
 }
